36cc776ac28f840b65c5049e8e251df02bdda2c0
YELRCY <YELRCY@hallgato.uniduna.hu>
2026-05-17 11:29:52 +0100

    Create MealForge ingredient and recipe dashboard

--- MealForgeApp/src/App.fs
+++ MealForgeApp/src/App.fs
@@ -2,13 +2,118 @@ module App
 
 open Browser.Dom
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
+type Ingredient =
+    {
+        Name: string
+        Unit: string
+        PricePerUnit: float
+    }
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+type RecipeIngredient =
+    {
+        IngredientName: string
+        Quantity: float
+    }
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+type Recipe =
+    {
+        Name: string
+        Ingredients: RecipeIngredient list
+        Portions: int
+    }
+
+let ingredients =
+    [
+        {
+            Name = "Chicken"
+            Unit = "kg"
+            PricePerUnit = 5.50
+        }
+
+        {
+            Name = "Rice"
+            Unit = "kg"
+            PricePerUnit = 2.20
+        }
+
+        {
+            Name = "Tomato"
+            Unit = "kg"
+            PricePerUnit = 3.10
+        }
+    ]
+
+let recipes =
+    [
+        {
+            Name = "Chicken Rice Bowl"
+            Portions = 10
+
+            Ingredients =
+                [
+                    {
+                        IngredientName = "Chicken"
+                        Quantity = 2.0
+                    }
+
+                    {
+                        IngredientName = "Rice"
+                        Quantity = 1.5
+                    }
+
+                    {
+                        IngredientName = "Tomato"
+                        Quantity = 1.0
+                    }
+                ]
+        }
+    ]
+
+let appContainer =
+    document.querySelector("#app")
+
+let ingredientHtml =
+    ingredients
+    |> List.map (fun ingredient ->
+        sprintf
+            """
+            <div class="ingredient-card">
+                <h3>%s</h3>
+                <p>Unit: %s</p>
+                <p>Price per unit: £%.2f</p>
+            </div>
+            """
+            ingredient.Name
+            ingredient.Unit
+            ingredient.PricePerUnit)
+    |> String.concat ""
+
+let recipeHtml =
+    recipes
+    |> List.map (fun recipe ->
+        sprintf
+            """
+            <div class="recipe-card">
+                <h2>%s</h2>
+                <p>Portions: %i</p>
+            </div>
+            """
+            recipe.Name
+            recipe.Portions)
+    |> String.concat ""
+
+appContainer.innerHTML <-
+    sprintf
+        """
+        <div class="container">
+            <h1>MealForge</h1>
+
+            <h2>Ingredients</h2>
+            %s
+
+            <h2>Recipes</h2>
+            %s
+        </div>
+        """
+        ingredientHtml
+        recipeHtml
